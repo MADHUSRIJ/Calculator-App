9f5477f523f1438ac0d7774aee5c18ce1a0c9814
Madhusri J <srimadhu.j@gmail.com>
2023-05-29 19:26:27 +0530

    [Update]. 2 UseStates

--- src/components/calculator_component/Calculator.js
+++ src/components/calculator_component/Calculator.js
@@ -4,36 +4,45 @@ import {ButtonsDiv} from './ButtonsDiv';
 import { evaluate } from 'mathjs';
 
 export function Calculator(){
-    const [value,setValue] = useState("");
+    const [inputValue,setInputValue] = useState("");
+    const [outputValue,setOutputValue] = useState("");
 
     const handleChange = (event) => {
         const input = event.target.value;
         const allowedCharacter = input.replace(/[^0-9+\-*/%]/g, ''); 
-        setValue(allowedCharacter || 0); 
+        setInputValue(allowedCharacter || 0); 
     };
 
     
     const handleButtonClick = (buttonValue) => {
         if(buttonValue === 'AC'){
-            setValue((prevValue) => prevValue='');
+            setInputValue((prevValue) => prevValue='');
+            setOutputValue((prevValue) => prevValue='');
         }
         else if (buttonValue === '=') {
-            setValue((prevValue) => {
-              const result = evaluate(prevValue); 
-              return String(result); 
+            setOutputValue((prevValue) => {
+              try{
+                const result = evaluate(inputValue); 
+                return String(result); 
+              }
+              catch(ex){
+                console.log(ex);
+                return prevValue;
+              }
             });
         }
         else if(buttonValue === '+/-'){
-            setValue((prevValue) => prevValue);
+            setInputValue((prevValue) => prevValue);
         }
         else{
-            setValue((prevValue) => prevValue === 0 ?  buttonValue : prevValue + buttonValue);
+            setInputValue((prevValue) => prevValue === 0 ?  buttonValue : prevValue + buttonValue);
         }
     };
 
     return(
         <div className='Main'>
-            <input id='text' className='TextField' value={value} onChange={handleChange} placeholder='0'/>
+            <input id='text' className='TextField' value={inputValue} onChange={handleChange} placeholder='0'/>
+            <input id='text' className='TextField' value={outputValue} onChange={handleChange} placeholder='0' readOnly/>
             <div className='Inputs'>
                 <ButtonsDiv handleButtonClick={handleButtonClick} />
             </div>
